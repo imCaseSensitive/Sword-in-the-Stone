c1f030cf51472172720db094bfad7f73e6327ea2
Matt Weidner <mweidner44@gmail.com>
2019-09-25 19:57:28 +0900

    All assets for this unity project

---
+++ Assets/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/CustomPoV.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPoV : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController charController;
+
+    [SerializeField] private AnimationCurve JumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode JumpKey;
+
+    private bool isJumping;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    private void PlayerMovement()
+    {
+        float vertInput = Input.GetAxis("Vertical") * movementSpeed;
+        float horizInput = Input.GetAxis("Horizontal") * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 sideMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + sideMovement);
+
+        JumpInput();
+    }
+
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(JumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpForce = JumpFallOff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+
+            yield return null;
+
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        isJumping = false;
+    }
+}
